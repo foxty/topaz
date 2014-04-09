d2efea7ab56bc53df349917141cd0e10fe0fa447
Isaac Tian <foxty@163.com>
2014-04-09 23:01:18 +0800

    Add result set handle to handle Model include mechanism. TODO!

---
+++ src/main/java/com/topaz/dao/TopazResultSetHandler.java
@@ -0,0 +1,18 @@
+package com.topaz.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.commons.dbutils.ResultSetHandler;
+
+public class TopazResultSetHandler implements ResultSetHandler<List<? extends BaseModel>> {
+
+	
+	public List<? extends BaseModel> handle(ResultSet rs) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
